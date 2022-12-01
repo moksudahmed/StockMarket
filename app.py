edb5a545ceebab833db70a4715feed1062824e86
Khondakar M Ahmed <k.moksud@gmail.com>
2022-12-01 21:50:35 +0600

    Up to date

--- app.py
+++ app.py
@@ -1,35 +1,65 @@
-import flask
-from flask import request, jsonify
+from flask import Flask
+import threading
+import time
+import markettiming
+from flask import Flask , request, jsonify
 import processdata
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-
-# Create some test data for our catalog in the form of a list of dictionaries.
-
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+import scraper_engine
+import log
+from time import sleep
 
 
-@app.route('/api/v1/resources/stock/all', methods=['GET'])
+my_stock_app = Flask(__name__)
+
+@my_stock_app.route('/')
+def hello_world():
+    return jsonify(processdata.isUpdated())
+
+@my_stock_app.route('/api/stock/all', methods=['GET'])
 def api_stockall():
-    processdata.isModified() 
+   # processdata.isModified() 
     return jsonify(processdata.get_stock_data())    
 
-@app.route('/api/v1/resources/stock/top_ten_gainer', methods=['GET'])
-def api_top_ten_gainer():
+@my_stock_app.route('/api/stock/details')
+def with_parameters():
+    trading_code = request.args.get('trading_code')
+    output = processdata.get_stock_details(trading_code)
+  #  print(output)
+    return jsonify(output)
+ 
+@my_stock_app.route('/api/stock/get_top10_gainer_data', methods=['GET'])
+def api_get_top10_gainer_data():
    # processdata.isModified() 
     return jsonify(processdata.get_top10_gainer_data())    
 
-@app.route('/api/v1/resources/stock/top_ten_loser', methods=['GET'])
-def api_top_ten_loser():
+@my_stock_app.route('/api/stock/get_top10_loser_data', methods=['GET'])
+def api_get_top10_loser_data():
    # processdata.isModified() 
     return jsonify(processdata.get_top10_loser_data())    
 
-@app.route('/api/v1/resources/stock/top_20_share', methods=['GET'])
-def api_top_20_share():
+@my_stock_app.route('/api/stock/get_top20_share_data', methods=['GET'])
+def api_get_top20_share_data():
    # processdata.isModified() 
     return jsonify(processdata.get_top20_share_data())    
 
-app.run()
+@my_stock_app.route('/api/stock/get_local_time', methods=['GET'])
+def api_get_local_time():
+    return jsonify(processdata.get_local_time())    
+
+def bot():
+    markettiming.determine_market_timming("09:25:00 AM", "02:30:00 PM")
+    """while True:
+            scraper_engine.scraper_engine()            
+            print("Data Scraped")
+            data = {"Data Scraped":markettiming.get_bd_current_local_time()}
+                    
+            log.write_json(data)
+            
+            time.sleep(30)
+       """     
+
+threading.Thread(target=bot).start()
+
+if __name__ == "__main__":
+    my_stock_app.run(use_reloader=False)
+
